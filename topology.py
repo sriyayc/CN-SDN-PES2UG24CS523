7734b98242fdbb7fde067c79e5a54bc86e133832
sriyayc <sriyayc@gmail.com>
2026-04-23 11:10:43 +0530

    Create topology.py

---
+++ topology.py
@@ -0,0 +1,32 @@
+from mininet.net import Mininet
+from mininet.node import RemoteController, OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.log import setLogLevel, info
+
+def custom_topology():
+    net = Mininet(controller=RemoteController, switch=OVSKernelSwitch, link=TCLink)
+
+    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6653)
+
+    s1 = net.addSwitch('s1')
+    s2 = net.addSwitch('s2')
+
+    h1 = net.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+    h2 = net.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
+    h3 = net.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
+    h4 = net.addHost('h4', ip='10.0.0.4/24', mac='00:00:00:00:00:04')
+
+    net.addLink(h1, s1, bw=10)
+    net.addLink(h2, s1, bw=10)
+    net.addLink(h3, s2, bw=10)
+    net.addLink(h4, s2, bw=10)
+    net.addLink(s1, s2, bw=100)
+
+    net.start()
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    custom_topology()
