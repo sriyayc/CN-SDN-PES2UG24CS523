d975b99bcfcd7e15ffe3f9b06e5277765307c48e
sriyayc <sriyayc@gmail.com>
2026-04-23 23:04:40 +0530

    Update topology.py

--- topology.py
+++ topology.py
@@ -1,32 +1,34 @@
+"""
+Mininet topology for the Traffic Monitoring project.
+1 OVS switch, 4 hosts, 100 Mbps links, remote POX controller on 127.0.0.1:6633
+"""
+
 from mininet.net import Mininet
 from mininet.node import RemoteController, OVSKernelSwitch
 from mininet.cli import CLI
 from mininet.link import TCLink
 from mininet.log import setLogLevel, info
 
-def custom_topology():
-    net = Mininet(controller=RemoteController, switch=OVSKernelSwitch, link=TCLink)
 
-    c0 = net.addController('c0', controller=RemoteController, ip='127.0.0.1', port=6633)
-
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
-
-    h1 = net.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
-    h2 = net.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
-    h3 = net.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
-    h4 = net.addHost('h4', ip='10.0.0.4/24', mac='00:00:00:00:00:04')
-
-    net.addLink(h1, s1, bw=10)
-    net.addLink(h2, s1, bw=10)
-    net.addLink(h3, s2, bw=10)
-    net.addLink(h4, s2, bw=10)
-    net.addLink(s1, s2, bw=100)
-
-    net.start()
+def build():
+    net = Mininet(controller=RemoteController, switch=OVSKernelSwitch,
+                  link=TCLink, autoSetMacs=True)
+    info('*** Adding controller\n')
+    c0 = net.addController('c0', controller=RemoteController,
+                           ip='127.0.0.1', port=6633)
+    info('*** Adding switch\n')
+    s1 = net.addSwitch('s1', protocols='OpenFlow10')
+    info('*** Adding hosts and 100 Mbps links\n')
+    for i in range(1, 5):
+        h = net.addHost('h%d' % i, ip='10.0.0.%d/24' % i)
+        net.addLink(h, s1, bw=100)
+    net.build()
+    c0.start()
+    s1.start([c0])
     CLI(net)
     net.stop()
 
+
 if __name__ == '__main__':
     setLogLevel('info')
-    custom_topology()
+    build()
